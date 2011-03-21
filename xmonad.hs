5f8922759cb6e97c442acd126d19af9771e75f96
Stephen Drodge <tehminkeh@gmail.com>
2011-03-21 03:08:16 -0400

    Changed some keybindings (workspaces and quit).

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,10 @@
 import XMonad
 import XMonad.Config.Gnome(gnomeConfig)
+import XMonad.Util.EZConfig(additionalKeysP,removeKeysP)
 import XMonad.Hooks.ManageHelpers(isFullscreen,doFullFloat)
 import XMonad.Layout.NoBorders(smartBorders)
 import XMonad.Actions.UpdatePointer
+import XMonad.Actions.CycleWS
 
 main = xmonad $ gnomeConfig
 	{ terminal = "xterm"
@@ -16,3 +18,10 @@ main = xmonad $ gnomeConfig
 		updatePointer (TowardsCentre 0.6 0.6)
 		logHook gnomeConfig
 	}
+	`additionalKeysP`
+	[ ("M-<Left>", prevWS)
+	, ("M-<Right>", nextWS)
+	, ("M-S-<Left>", shiftToPrev)
+	, ("M-S-<Right>", shiftToNext)
+	]
+	`removeKeysP` ["M-S-q"]
