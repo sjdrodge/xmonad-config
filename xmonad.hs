a345879829b62f470ab97f518f7b7a89fd37a2c8
Stephen Drodge <tehminkeh@gmail.com>
2011-03-20 22:27:59 -0400

    Removed borders from fullscreen windows.

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,12 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.ManageHelpers(isFullscreen,doFullFloat)
+import XMonad.Layout.NoBorders(smartBorders)
 
 main = xmonad $ gnomeConfig
 	{ terminal = "xterm"
 	, modMask = mod4Mask
+	, layoutHook = smartBorders (layoutHook gnomeConfig)
 	, manageHook = composeAll
 		[ manageHook gnomeConfig
 		, isFullscreen --> doFullFloat
