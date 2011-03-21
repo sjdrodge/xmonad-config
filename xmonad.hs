016069471bac7620410a1d12ed2a83a8d43f6dbd
Stephen Drodge <tehminkeh@gmail.com>
2011-03-20 22:14:31 -0400

    Fullscreen fix.

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,13 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Util.EZConfig
+import XMonad.Hooks.ManageHelpers
 
 main = xmonad $ gnomeConfig
 	{ terminal = "xterm"
 	, modMask = mod4Mask
+	, manageHook = composeAll
+		[ manageHook gnomeConfig
+		, isFullscreen --> doFullFloat
+		]
 	}
