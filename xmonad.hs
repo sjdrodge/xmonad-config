70a3016413fe7bd94c78c54fcf80ae9486d7ddc8
Stephen Drodge <tehminkeh@gmail.com>
2011-03-24 04:16:14 -0400

    Converted short keybinding descriptors to standard ones.

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,13 @@
 import XMonad
 import XMonad.Config.Desktop(desktopLayoutModifiers)
 import XMonad.Config.Gnome(gnomeConfig)
-import XMonad.Util.EZConfig(additionalKeysP,removeKeysP)
+import XMonad.Util.EZConfig(additionalKeys,removeKeys)
 import XMonad.Hooks.ManageHelpers(isFullscreen,doFullFloat)
 import XMonad.Layout.NoBorders(smartBorders)
 import XMonad.Actions.CycleWS(prevWS,nextWS,shiftToPrev,shiftToNext)
 import XMonad.Actions.UpdatePointer
 
 myTerminal = "xterm"
-myModMask = mod4Mask
 
 myLayouts = (Tall 1 0.01 0.7) ||| Full
 
@@ -18,17 +17,24 @@ myManageHook = isFullscreen --> doFullFloat
 
 myLogHook = updatePointer (TowardsCentre 0.6 0.6)
 
-myAdditionalKeysP =
-	[ ("M-<Left>", prevWS)
-        , ("M-<Right>", nextWS)
-        , ("M-S-<Left>", shiftToPrev)
-        , ("M-S-<Right>", shiftToNext)
-	, ("M-M1-<Left>" , shiftToPrev >> prevWS)
-	, ("M-M1-<Right>", shiftToNext >> nextWS)
-        ]
+-- Keybindings --
 
-myRemoveKeysP = ["M-S-q"]
+myModMask = mod4Mask
 
+myAdditionalKeys =
+	[ ((myModMask, xK_Left), prevWS)
+	, ((myModMask, xK_Right), nextWS)
+	, ((myModMask .|. shiftMask, xK_Left), shiftToPrev)
+	, ((myModMask .|. shiftMask, xK_Right), shiftToNext)
+	, ((myModMask .|. mod1Mask, xK_Left), shiftToPrev >> prevWS)
+	, ((myModMask .|. mod1Mask, xK_Right), shiftToNext >> nextWS)
+	]
+
+myRemoveKeys =
+	[ (myModMask .|. shiftMask, xK_q)
+	]
+
+-- End Keybindings --
 
 main = xmonad $ gnomeConfig
 	{ terminal = myTerminal
@@ -37,5 +43,5 @@ main = xmonad $ gnomeConfig
 	, manageHook = myManageHook <+> manageHook gnomeConfig
 	, logHook = myLogHook >> logHook gnomeConfig
 	}
-	`additionalKeysP` myAdditionalKeysP
-	`removeKeysP` myRemoveKeysP
+	`additionalKeys` myAdditionalKeys
+	`removeKeys` myRemoveKeys
