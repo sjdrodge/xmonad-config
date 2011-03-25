b8e8fcff2e545172e6aa52dce92de34800e40fc1
Stephen Drodge <tehminkeh@gmail.com>
2011-03-25 09:40:22 -0400

    Workspaces now organized in a 3x3 grid (keybindings).

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,14 @@
+import qualified Data.Map as M
+
 import XMonad
 import XMonad.Config.Desktop(desktopLayoutModifiers)
 import XMonad.Config.Gnome(gnomeConfig)
-import XMonad.Util.EZConfig(additionalKeys,removeKeys)
 import XMonad.Hooks.ManageHelpers(isFullscreen,doFullFloat)
 import XMonad.Layout.NoBorders(smartBorders)
-import XMonad.Actions.CycleWS(prevWS,nextWS,shiftToPrev,shiftToNext)
 import XMonad.Actions.Promote(promote)
+import XMonad.Util.CustomKeys(customKeysFrom)
 import XMonad.Actions.UpdatePointer
+import XMonad.Actions.Plane
 
 myTerminal = "xterm"
 
@@ -22,28 +24,20 @@ myLogHook = updatePointer (TowardsCentre 0.6 0.6)
 
 myModMask = mod4Mask
 
-myAdditionalKeys =
-	[ ((myModMask, xK_Return), promote)
-	, ((myModMask, xK_Left), prevWS)
-	, ((myModMask, xK_Right), nextWS)
-	, ((myModMask .|. shiftMask, xK_Left), shiftToPrev)
-	, ((myModMask .|. shiftMask, xK_Right), shiftToNext)
-	, ((myModMask .|. mod1Mask, xK_Left), shiftToPrev >> prevWS)
-	, ((myModMask .|. mod1Mask, xK_Right), shiftToNext >> nextWS)
-	]
+myAdditionalKeys _ =
+	[ ((myModMask, xK_Return), promote) ]
+	++ M.assocs (planeKeys myModMask (Lines 3) Finite)
 
-myRemoveKeys =
-	[ (myModMask .|. shiftMask, xK_q)
-	]
+myRemoveKeys _ =
+	[ (myModMask .|. shiftMask, xK_q) ]
 
 -- End Keybindings --
 
 main = xmonad $ gnomeConfig
 	{ terminal = myTerminal
 	, modMask = myModMask
+	, keys = customKeysFrom gnomeConfig myRemoveKeys myAdditionalKeys
 	, layoutHook = myLayoutHook
 	, manageHook = myManageHook <+> manageHook gnomeConfig
 	, logHook = myLogHook >> logHook gnomeConfig
 	}
-	`additionalKeys` myAdditionalKeys
-	`removeKeys` myRemoveKeys
