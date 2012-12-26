1791197e83c868d4d76df785347f6f62dba7cfbc
Stephen Drodge <stephen.drodge@gmail.com>
2012-12-25 20:42:17 -0500

    Removed use of xmonad-statusbar-applet

--- xmonad.hs
+++ xmonad.hs
@@ -1,21 +1,11 @@
--- This configuration is intended to be used with the XMonad Statusbar Applet
--- available at <https://github.com/tehminkeh/xmonad-statusbar-applet>.
--- It also requires the following Haskell packages:
--- dbus-core: <http://hackage.haskell.org/package/dbus-core>
--- dbus-client: <http://hackage.haskell.org/package/dbus-client>
--- pango: <http://hackage.haskell.org/package/pango>
-
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M(assocs)
-import Data.String(fromString)
-import qualified Data.Text.Lazy.Encoding as TL(decodeUtf8)
 import Control.Monad(liftM)
 
 import XMonad
 import XMonad.Config.Desktop(desktopLayoutModifiers)
 import XMonad.Config.Gnome(gnomeConfig)
-import XMonad.Hooks.ManageHelpers(isFullscreen,doFullFloat)
-import XMonad.Hooks.DynamicLog(defaultPP,dynamicLogWithPP,PP(..))
+import XMonad.Hooks.ManageHelpers(isFullscreen)
 import XMonad.Hooks.UrgencyHook(focusUrgent,withUrgencyHook,NoUrgencyHook(..))
 import XMonad.Hooks.FadeInactive(isUnfocused,fadeOutLogHook)
 import XMonad.Layout.NoBorders(lessBorders,SetsAmbiguous(hiddens))
@@ -33,15 +23,6 @@ import XMonad.Actions.Plane(planeKeys,Lines(Lines),Limits(Finite))
 import XMonad.Actions.UpdatePointer
     (updatePointer,PointerPosition(TowardsCentre))
 
-import DBus.Types(toVariant)
-import DBus.Bus(getSessionBus)
-import DBus.Message(Signal(..))
-import DBus.Client(send_,newClient,runDBus,Client,DBus)
-
-import Graphics.Rendering.Pango.Enums(Weight(..))
-import Graphics.Rendering.Pango.Markup(markSpan,SpanAttribute(..))
-import Graphics.Rendering.Pango.Layout(escapeMarkup)
-
 -- Terminal --
 myTerminal = "~/bin/urxvtcd"
 
@@ -56,35 +37,6 @@ myManageHook = composeAll
     ]
 
 -- LogHook --
-sendUpdateSignal :: String -> DBus ()
-sendUpdateSignal output = send_ Signal
-    { signalPath = fromString "/org/xmonad/Log"
-    , signalMember = fromString "Update"
-    , signalInterface = fromString "org.xmonad.Log"
-    , signalDestination = Nothing
-    , signalBody = [toVariant (TL.decodeUtf8 (fromString output))]
-    }
-
-myPrettyPrinter :: Client -> PP
-myPrettyPrinter client = defaultPP
-    { ppOutput  = runDBus client . sendUpdateSignal
-    , ppTitle   = escapeMarkup
-    , ppCurrent = markSpan
-        [ FontWeight WeightBold
-        , FontForeground "green"
-        ] . escapeMarkup
-    , ppVisible = const ""
-    , ppHidden  = escapeMarkup
-    , ppLayout  = markSpan
-        [ FontWeight WeightBold
-        , FontForeground "white"
-        ] . escapeMarkup
-    , ppUrgent  = markSpan
-        [ FontWeight WeightBold
-        , FontForeground "red"
-        ]
-    }
-
 fadeRules :: Query Rational
 fadeRules = do
     fullscreen <- isFullscreen
@@ -93,9 +45,7 @@ fadeRules = do
                           | focused -> 0.85
                           | otherwise -> 0.8
 
-myLogHook :: Client -> X ()
-myLogHook client = do
-    dynamicLogWithPP (myPrettyPrinter client)
+myLogHook = do
     updatePointer (TowardsCentre 0.6 0.6)
     fadeOutLogHook fadeRules
 
@@ -142,7 +92,6 @@ myRemoveKeys _ =
 
 -- Apply settings --
 main = do
-    dbusClient <- newClient =<< getSessionBus
     xmonad $ withUrgencyHook NoUrgencyHook $ gnomeConfig
         { terminal = myTerminal
         , workspaces = myWorkspaces
@@ -150,6 +99,6 @@ main = do
         , keys = customKeysFrom gnomeConfig myRemoveKeys myAdditionalKeys
         , layoutHook = myLayoutHook
         , manageHook = myManageHook <+> manageHook gnomeConfig
-        , logHook = myLogHook dbusClient >> logHook gnomeConfig
+        , logHook = myLogHook >> logHook gnomeConfig
         , handleEventHook = fullscreenEventHook <+> handleEventHook gnomeConfig
         }
