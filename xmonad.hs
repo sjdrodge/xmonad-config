8b2a9391e853c94d5324e74f1837c44c399e34be
Stephen Drodge <stephen.drodge@gmail.com>
2013-05-17 10:47:05 -0400

    Decrease transparency.

--- xmonad.hs
+++ xmonad.hs
@@ -47,8 +47,8 @@ fadeRules = do
     fullscreen <- isFullscreen
     unfocused <- isUnfocused
     return $ case () of _ | not whitelisted || fullscreen -> 1
-                          | not unfocused -> 0.9
-                          | otherwise -> 0.85
+                          | not unfocused -> 0.95
+                          | otherwise -> 0.9
 
 myLogHook = do
     updatePointer (TowardsCentre 0.6 0.6)
