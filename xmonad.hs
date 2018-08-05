291120a74c249f3f60c59607f4cf5b25c152ca0c
Stephen Drodge <stephen.drodge@gmail.com>
2018-08-05 01:38:03 -0400

    Update Hangouts title.

--- xmonad.hs
+++ xmonad.hs
@@ -58,7 +58,7 @@ myWebLayouts = Tall { tallNMaster = 1
                     }
 
 myCommLayouts = named "Comm"
-    $ reflectHoriz $ withIM ( 0.20 ) ( Title "Hangouts" )
+    $ reflectHoriz $ withIM ( 0.20 ) ( Title "Google Hangouts" )
     $ combineTwoP ( reflectVert $ Mirror $ TwoPane 0.01 0.45) (Mirror Accordion) Grid ( Resource "crx_nckgahadagoaajjgafhacjanaoiihapd" )
 --    ( Mirror $ myWebLayouts { tallRatio = 0.55 } )
 
